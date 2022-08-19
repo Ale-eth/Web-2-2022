85d529a1226bd7e3b8db31ed0aac56b842a4369c
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-08-19 19:13:25 -0300

    Create 1.php

---
+++ Practicos/1/1.php
@@ -0,0 +1,20 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+        echo "<h1>Hola Mundo!</h1>";
+        ?>
+    </body>
+    </html>
+
+    <!-- 
+    a) La extension debe tener terminacion .php
+    b) Es una pagina estatica
+    c) Por que un servidor tiene apache, y el modulo interprete de php
+    <--!
