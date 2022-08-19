f721bdd4d79db46365dadd37b5c6257fcf8511c1
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-08-19 16:57:16 -0300

    Update 01.php

--- Teoria/01.php
+++ Teoria/01.php
@@ -1,1 +1,15 @@
 
+<?php
+//Operaciones con arreglos:
+
+  // Insertar un elemento al final
+  array_push($arreglo, $valor);
+  //Extraer el último elemento del array
+  array_pop($arreglo);
+  // Invertir orden de los elementos (Devuelve un arreglo nuevo)
+  array_reverse($arreglo);
+  // Operaciones aritméticas sobre arreglos:
+  array_sum($arr); // Suma los valores del arreglo
+  array_product($arr); // Multiplica los valores del arreglo
+  
+?>
