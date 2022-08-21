d54f5f55c4f60b5a6db02fe750448fc3af997d26
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-08-20 22:00:31 -0300

    Delete Practicos/1/8 directory

--- Practicos/1/8/pi.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "
-        <h2> Numero PI π </h2>
-        <p> π (pi) es la relación entre la longitud de una circunferencia y su diámetro en geometría euclidiana.1​ Es un número irracional2​ y una de las constantes matemáticas más importantes.</p>
-        <p>Se emplea frecuentemente en matemáticas, física e ingeniería. El valor numérico de π, truncado a sus primeras cifras, es el siguiente:3</p>
-        <p>El valor de π se ha obtenido con diversas aproximaciones a lo largo de la historia, siendo una de las constantes matemáticas que más aparece en las ecuaciones de la física,</p>
-        <p>junto con el número e. Cabe destacar que el cociente entre la longitud de cualquier circunferencia y la de su diámetro no es constante en geometrías no euclidianas. </p>
-";
-?>
